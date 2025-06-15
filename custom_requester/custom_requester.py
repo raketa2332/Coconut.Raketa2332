03b0c3853feb48081556b3c8c6a04a45e6323dc5
= <ryzhenko@informatics.ru>
2025-06-15 20:48:36 +0300

    test: поправил логирование внутри CustomRequester

--- custom_requester/custom_requester.py
+++ custom_requester/custom_requester.py
@@ -31,25 +31,74 @@ class CustomRequester:
         self.headers.update(kwargs)
         self.session.headers.update(self.headers)
 
+    # def log_request_and_response(self, response):
+    #     try:
+    #         request = response.request
+    #         GREEN = "\033[32m"
+    #         RED = "\033[31m"
+    #         RESET = "\033[0m"
+    #         headers = "\\\n".join([f"-H'{header}: {value}'" for header, value in request.headers.items()])
+    #         full_test_name = f"pytest {os.environ.get('PYTEST_CURRENT_TEST', '').replace('(call)', '')}"
+    #
+    #         body = ""
+    #         if hasattr(request, "body") and request.body is not None:
+    #             if isinstance(request.body, bytes):
+    #                 body = request.body.decode("utf-8")
+    #             body = f"-d '{body}' \n" if body != "{}" else ""
+    #
+    #         # Логируем запрос
+    #         self.logger.info(f"\n{'=' * 40} REQUEST {'=' * 40}")
+    #         self.logger.info(
+    #             f"{GREEN}{full_test_name}{RESET}\ncurl -X {request.method} '{request.url}'{headers} \\\n{body}"
+    #         )
+    #
+    #         # Обрабатываем ответ
+    #         response_status = response.status_code
+    #         is_success = response.ok
+    #         response_data = response.text
+    #
+    #         # Попытка форматировать JSON
+    #         try:
+    #             response_data = json.dump(json.load(response.text), indent=4, ensure_ascii=False)
+    #         except json.JSONDecodeError:
+    #             pass  # Оставляем текст, если это не JSON
+    #
+    #         # Логируем ответ
+    #         self.logger.info(f"\n{'=' * 40} RESPONSE {'=' * 40}")
+    #         if not is_success:
+    #             self.logger.info(f"\nSTATUS_CODE: {RED}{response_status}{RESET}\nDATA: {RED}{response_data}{RESET}")
+    #         else:
+    #             self.logger.info(f"\nSTATUS_CODE: {GREEN}{response_status}{RESET}\nDATA:\n{response_data}")
+    #
+    #         self.logger.info(f"{'=' * 80}\n")
+    #     except Exception as e:
+    #         self.logger.error(f"\nLogging failed {type(e)} - {e}")
     def log_request_and_response(self, response):
+        """
+        Логирование запросов и ответов.
+        :param response: Объект ответа requests.Response.
+        """
         try:
             request = response.request
-            GREEN = "\033[32m"
-            RED = "\033[31m"
-            RESET = "\033[0m"
-            headers = "\\\n".join([f"-H'{header}: {value}'" for header, value in request.headers.items()])
-            full_test_name = f"pytest {os.environ.get('PYTEST_CURRENT_TEST', '').replace('(call)', '')}"
+            GREEN = '\033[32m'
+            RED = '\033[31m'
+            RESET = '\033[0m'
+            headers = " \\\n".join([f"-H '{header}: {value}'" for header, value in request.headers.items()])
+            full_test_name = f"pytest {os.environ.get('PYTEST_CURRENT_TEST', '').replace(' (call)', '')}"
 
             body = ""
-            if hasattr(request, "body") and request.body is not None:
+            if hasattr(request, 'body') and request.body is not None:
                 if isinstance(request.body, bytes):
-                    body = request.body.decode("utf-8")
-                body = f"-d '{body}' \n" if body != "{}" else ""
+                    body = request.body.decode('utf-8')
+                body = f"-d '{body}' \n" if body != '{}' else ''
 
             # Логируем запрос
             self.logger.info(f"\n{'=' * 40} REQUEST {'=' * 40}")
             self.logger.info(
-                f"{GREEN}{full_test_name}{RESET}\ncurl -X {request.method} '{request.url}'{headers} \\\n{body}"
+                f"{GREEN}{full_test_name}{RESET}\n"
+                f"curl -X {request.method} '{request.url}' \\\n"
+                f"{headers} \\\n"
+                f"{body}"
             )
 
             # Обрабатываем ответ
@@ -59,17 +108,22 @@ class CustomRequester:
 
             # Попытка форматировать JSON
             try:
-                response_data = json.dump(json.load(response.text), indent=4, ensure_ascii=False)
+                response_data = json.dumps(json.loads(response.text), indent=4, ensure_ascii=False)
             except json.JSONDecodeError:
                 pass  # Оставляем текст, если это не JSON
 
             # Логируем ответ
             self.logger.info(f"\n{'=' * 40} RESPONSE {'=' * 40}")
             if not is_success:
-                self.logger.info(f"\nSTATUS_CODE: {RED}{response_status}{RESET}\nDATA: {RED}{response_data}{RESET}")
+                self.logger.info(
+                    f"\tSTATUS_CODE: {RED}{response_status}{RESET}\n"
+                    f"\tDATA: {RED}{response_data}{RESET}"
+                )
             else:
-                self.logger.info(f"\nSTATUS_CODE: {GREEN}{response_status}{RESET}\nDATA:\n{response_data}")
-
+                self.logger.info(
+                    f"\tSTATUS_CODE: {GREEN}{response_status}{RESET}\n"
+                    f"\tDATA:\n{response_data}"
+                )
             self.logger.info(f"{'=' * 80}\n")
         except Exception as e:
-            self.logger.error(f"\nLogging failed {type(e)} - {e}")
+            self.logger.error(f"\nLogging failed: {type(e)} - {e}")
